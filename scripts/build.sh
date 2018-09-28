3432a0556e4e8188b00352e44fceb2e084a1aece
elrot <kamil@fogger.io>
2018-09-28 22:17:47 +0200

    initial

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+# task script is in resource-tutorial/job-inputs/ folder
+# application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
+# $GOPATH is gopath/ folder
+export GOPATH=$(pwd)/gopath
+cd gopath/src/github.com/elrot/fgr/
+
+GOOS=linux GOARCH=x86_64 go build -o .build/fogger cmd/foggerd/*.go
