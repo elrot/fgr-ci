d67c659dbd68883cb77986b55408af290ca6521b
elrot <kamil@fogger.io>
2018-09-29 18:47:06 +0200

    print binaries

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,10 @@ set -e
 # task script is in resource-tutorial/job-inputs/ folder
 # application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
 # $GOPATH is gopath/ folder
-export GOPATH=$(pwd)/gopath
+ROOT=$(pwd)
+export GOPATH=$ROOT/gopath
 
-cd gopath/src/github.com/elrot/fgr/
+cd $ROOT/gopath/src/github.com/elrot/fgr/
 gx install
-GOOS=linux GOARCH=amd64 go build -o .build/fogger cmd/foggerd/*.go
+GOOS=linux GOARCH=amd64 go build -o $ROOT/fogger-binaries cmd/foggerd/*.go
+
