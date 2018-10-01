97b7c945c2d48bf25431298d12ca6fe778f279af
elrot <kamil@fogger.io>
2018-10-01 23:15:27 +0200

    build tarballs

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,18 @@
 set -e
 
 ROOT=$(pwd)
+BIN=$ROOT/mister-binaries/mister/
+
+
 export GOPATH=$ROOT/gopath
 
 cd $ROOT/gopath/src/github.com/elrot/mister/
 gx install
-GOOS=linux GOARCH=amd64 go build -o $ROOT/mister-binaries/misterd cmd/misterd/*.go
 
+DATE=$(date '+%Y%m%d%H%M%S')
+COMMIT=$(git rev-parse --short HEAD)
+
+make build-tarbal GOOS=linux GOARCH=386 TARBAL=$BIN/linux/static/nightly/x86/mister-0.0.0-$DATE-$COMMIT.tar.gz
+make build-tarbal GOOS=linux GOARCH=amd64 TARBAL=$BIN/linux/static/nightly/x86_64/mister-0.0.0-$DATE-$COMMIT.tar.gz
+make build-tarbal GOOS=linux GOARCH=arm TARBAL=$BIN/linux/static/nightly/armhf/mister-0.0.0-$DATE-$COMMIT.tar.gz
+make build-tarbal GOOS=linux GOARCH=arm64 TARBAL=$BIN/linux/static/nightly/aarch64/mister-0.0.0-$DATE-$COMMIT.tar.gz
